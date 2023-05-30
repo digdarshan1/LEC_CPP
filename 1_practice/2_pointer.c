b6c7ef9f7103d634a9aa86807d35971e2314a079
Digdarshan thapa <tdigdarhsan1@gmail.com>
2023-05-30 13:12:22 +0545

    assignment

---
+++ 1_practice/2_pointer.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+   void main(){
+    int* pc, c;
+    c=5;
+    pc= &c;
+    printf("value of address of c is given by pointer variable %p",pc);
+    printf("value of value of b is given by *pc i.e %d", *pc);
+   }
